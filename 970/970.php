67cfba766575b14b3406e1d09f27bdbe0b977509
Tun Dz <33826364+hotungho92@users.noreply.github.com>
2019-09-16 13:59:14 +0700

    Chỉnh sửa
    Sửa tên

--- 970/970.php
+++ 970/970.php
@@ -2,10 +2,12 @@
 
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>Block 970 </title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+        <link href="css/swiper.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        
             
         <?php
         if (!class_exists('lessc')) {
@@ -19,57 +21,15 @@
         <script src="js/jquery-1.8.0.min.js" type="text/javascript"></script>
         <script src="js/jquery.movingboxes.js" type="text/javascript"></script>
         <script src="js/script.js" type="text/javascript"></script>
+        <script src="js/swiper.min.js" type="text/javascript"></script>
+       
 
 
     </head>
 
     <body>
-        <div class="mod_custom_27">
-            <div id="wrapper">
-
-                <!-- MovingBoxes Slider -->
-                <ul id="slider">
-
-                    <li>
-                        <img src="images/1.jpg" alt="picture">
-                        <h2>News Heading</h2>
-                        <p>Add a short exerpt here... <a href="#">more</a></p>
-                    </li>
-
-                    <li>
-                        <img src="images/2.jpg" alt="picture">
-                        <h2>News Heading</h2>
-                        <p>Add a short exerpt here... <a href="#">more</a> and a whole lot more text goes here, so we can see the height adjust.</p>
-                    </li>
-
-                    <li>
-                        <img src="images/3.jpg" alt="picture">
-                        <h2>News Heading</h2>
-                        <p>Add a short exerpt here... <a href="#">more</a></p>
-                    </li>
-
-                    <li>
-                        <img src="images/4.jpg" alt="picture">
-                        <h2>News Heading</h2>
-                        <p>Add a short exerpt here... <a href="#">more</a></p>
-                    </li>
-
-                    <li>
-                        <img src="images/5.jpg" alt="picture">
-                        <h2>News Heading</h2>
-                        <p>Add a short exerpt here... <a href="#">more</a></p>
-                    </li>
-
-                    <li>
-                        <img src="images/6.jpg" alt="picture">
-                        <h2>News Heading</h2>
-                        <p>Add a short exerpt here... <a href="#">more</a></p>
-                    </li>
-
-                </ul><!-- end Slider #1 -->
-
-            </div><!-- end wrapper -->
-        </div>
+    
+     <?php include '../970/970-content.php';?>
     </body>
 
 </html>
