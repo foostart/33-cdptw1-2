f46cc609b0b382068a9f5b9c2908384c0e3894c3
Tun Dz <33826364+hotungho92@users.noreply.github.com>
2019-09-26 09:34:54 +0700

    chinh sua

--- 1639/1990-content.php
+++ 1639/1990-content.php
@@ -113,19 +113,6 @@
 	</footer>
 	<!--end foot-->
 	</section>
-	<script>
-    var swiper = new Swiper('.swiper-container', {
-      // Enable lazy loading
-      lazy: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  </script>
+	
 </body>
 
