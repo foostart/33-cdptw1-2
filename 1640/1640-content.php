449ab5bc65d84d8442cfb2b6b6023fc387fdd639
Tun Dz <33826364+hotungho92@users.noreply.github.com>
2019-11-01 08:19:00 +0700

    cap nhat chinh sua

--- 1640/1640-content.php
+++ 1640/1640-content.php
@@ -1,113 +1,103 @@
-<body>
-	<section class="type-1640">
-		<!--header-->
-		<header class="top-header">
-			<div class="container">
-				<div class="row">
-					<!--About-->
-					<div class="container">
-						<div class="About">
-							<h1>About Us</h1><br>
-							<div class="row">
-								<div class="col-md-6 col-xs-12">
-									<h2>Eum Fugiat Quo Volutas Nullatur </h2><br>
-									<h5>Taque oarum hic tentur a saplente delectus ,ut aut reiciendis voluptatibus
-										maiores alias conse quatur aut perferendis doloribus asperiores repellat.</h5>
-									<br>
-									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur
-										Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus
-										saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.</p>
-									<br>
-									<h2>Eum Fugiat Quo Volutas Nullatur </h2><br>
-									<h5>Taque oarum hic tentur a saplente delectus ,ut aut reiciendis voluptatibus
-										maiores alias conse quatur aut perferendis doloribus asperiores repellat.</h5>
-									<br>
-									<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur
-										Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus
-										saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.</p>
-									<br>
-								</div>
-								<div class="col-md-6 col-xs-12 hinh">
-									<img src="images/1.jpg" alt="" class="img-responsive">
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
-		<!--end About-->
-		<!--New -->
-		<div class="container">
-			<div class="New">
-				<h1>New & Event</h1>
-				<div class="row">
-				<div class="col-md-6 col-sm-12 col-xs-12">
+<div class="type-1640">
+    <header class="top-header">
+        <div class="container">
+            <div class="row">
+                <div class="container">
+                    <div class="About">
+                        <h1>About Us</h1><br>
+                        <div class="row">
+                            <div class="col-md-6 col-xs-12">
+                                <h2>Eum Fugiat Quo Volutas Nullatur </h2><br>
+                                <h5>Taque oarum hic tentur a saplente delectus ,ut aut reiciendis voluptatibus
+                                    maiores alias conse quatur aut perferendis doloribus asperiores repellat.</h5>
+                                <br>
+                                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur
+                                    Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus
+                                    saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.</p>
+                                <br>
+                                <h2>Eum Fugiat Quo Volutas Nullatur </h2><br>
+                                <h5>Taque oarum hic tentur a saplente delectus ,ut aut reiciendis voluptatibus
+                                    maiores alias conse quatur aut perferendis doloribus asperiores repellat.</h5>
+                                <br>
+                                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur
+                                    Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus
+                                    saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.</p>
+                                <br>
+                            </div>
+                            <div class="col-md-6 col-xs-12 hinh">
+                                <img src="images/1.jpg" alt="" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="New">
+            <h1>New & Event</h1>
+            <div class="row">
+                <div class="col-md-6 col-sm-12 col-xs-12">
 
-					<div class="col-md-6 col-sm-6 col-xs-6">
-						<img src="images/8.jpg" alt="" class="img-responsive">
-					</div>
-					<div class="col-md-6 col-sm-6 col-xs-6">
-						<h4>24th<br>
-							July</h4>
-						<p>Temporibus autem quibusdam et<br>
-							aut officiis debitis aut rerum<br>
-							necessitatibus saepe eveniet ut<br>
-							et voluptates repudiandae.</p>
-					</div>
-					</div>
-					<div class="col-md-6 col-sm-12 col-xs-12">
-					<div class="col-md-6 col-sm-6 col-xs-6">
-						<img src="images/9.jpg" alt="" class="img-responsive">
-					</div>
-					<div class="col-md-6 col-sm-6 col-xs-6">
-						<h4>24th<br>
-							July</h4>
-						<p>Temporibus autem quibusdam et<br>
-							aut officiis debitis aut rerum<br>
-							necessitatibus saepe eveniet ut<br>
-							et voluptates repudiandae.</p><br>
-					</div>
-					</div>
-					<!--end New-->
-				</div>
-			</div>
-		</div>
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <img src="images/8.jpg" alt="" class="img-responsive">
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <h4>24th<br>
+                            July</h4>
+                        <p>Temporibus autem quibusdam et<br>
+                            aut officiis debitis aut rerum<br>
+                            necessitatibus saepe eveniet ut<br>
+                            et voluptates repudiandae.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <img src="images/9.jpg" alt="" class="img-responsive">
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <h4>24th<br>
+                            July</h4>
+                        <p>Temporibus autem quibusdam et<br>
+                            aut officiis debitis aut rerum<br>
+                            necessitatibus saepe eveniet ut<br>
+                            et voluptates repudiandae.</p><br>
+                    </div>
+                </div>
 
-		<!--Event-->
-		<div class="container">
-			<div class="Event">
-				<div class="row">
-				<div class="col-md-6 col-sm-12 col-xs-12">
-						<div class="col-md-6 col-sm-6 col-xs-6">
-						<img src="images/7.jpg" alt="" class="img-responsive">
-						
-					</div>
+            </div>
+        </div>
 
-				<div class="col-md-6 col-sm-6 col-xs-6">
-						<h4>24th<br>
-							July</h4>
-						<p>Temporibus autem quibusdam et<br>
-							aut officiis debitis aut rerum<br>
-							necessitatibus saepe eveniet ut<br>
-							et voluptates repudiandae.</p>
-					</div>
-					</div>
-					<div class="col-md-6 col-sm-12 col-xs-12">
-						<div class="col-md-6 col-sm-6 col-xs-6">
-						<img src="images/10.jpg" alt="" class="img-responsive">
-					</div>
-					<div class="col-md-6 col-sm-6 col-xs-6">
-						<h4>24th<br>
-							July</h4>
-						<p>Temporibus autem quibusdam et<br>
-							aut officiis debitis aut rerum<br>
-							necessitatibus saepe eveniet ut<br>
-							et voluptates repudiandae.</p>
-					</div>
-				</div>
-			</div>
-		</div>
+        <div class="Event">
+            <div class="row">
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <img src="images/7.jpg" alt="" class="img-responsive">
 
-	</section>
-</body>
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <h4>24th<br>
+                            July</h4>
+                        <p>Temporibus autem quibusdam et<br>
+                            aut officiis debitis aut rerum<br>
+                            necessitatibus saepe eveniet ut<br>
+                            et voluptates repudiandae.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <img src="images/10.jpg" alt="" class="img-responsive">
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <h4>24th<br>
+                            July</h4>
+                        <p>Temporibus autem quibusdam et<br>
+                            aut officiis debitis aut rerum<br>
+                            necessitatibus saepe eveniet ut<br>
+                            et voluptates repudiandae.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- 1640/1640.php
+++ 1640/1640.php
@@ -1,26 +1,24 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">  
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Module 1640</title>
-  
-        <meta name="viewport" content="user-scalable=0, initial-scale=1.0">
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/1640.css" rel="stylesheet" type="text/css" />
-        <script src ="js/bootstrap.min.js" ></script>
-        <?php
+    <meta name="viewport" content="user-scalable=0, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/1640.css" rel="stylesheet" type="text/css" />
+    <?php
         if (!class_exists('lessc')) {
             include ('./libs/lessc.inc.php');
         }
         $less = new lessc;
         $less->compileFile('less/1640.less', 'css/1640.css');
         ?>
-        
-       
+
 </head>
 <?php include '../1640/1640-content.php'; ?>
+
 </html>
